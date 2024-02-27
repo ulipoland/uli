b2e9a3e2be76f1e084acd612a10563c192582e76
ulipoland <131090559+ulipoland@users.noreply.github.com>
2024-02-27 23:45:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                url: "https://ulipoland.github.io/uli/PS2311_agenda.pdf",
+                url: "https://ulipoland.github.io/uli/PS2402_agenda.pdf",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 headers: [
@@ -45,7 +45,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         /* Pass meta data of file */
         metaData: {
             /* file name */
-            fileName: "PS2311_agenda.pdf"
+            fileName: "PS2402_agenda.pdf"
         }
     }, viewerConfig);
 });
